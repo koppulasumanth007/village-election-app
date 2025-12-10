8f1694260562c09f81feccd157c98d1641e840c9
sumanth koppula <148244918+koppulasumanth007@users.noreply.github.com>
2025-12-10 11:38:04 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdaDv7Jajsi-5m4MzeXjW0N98R-b3N55Q",
-  authDomain: "village-election-app.firebaseapp.com",
-  projectId: "village-election-app",
-  storageBucket: "village-election-app.firebasestorage.app",
-  messagingSenderId: "310772940698",
-  appId: "1:310772940698:web:17848d07b81390bc430423",
-  measurementId: "G-JFXW8LFMNW"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
