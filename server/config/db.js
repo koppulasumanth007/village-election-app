7ce80a23ffa41137e830469985dbaffb5252fc98
sumanth koppula <148244918+koppulasumanth007@users.noreply.github.com>
2025-12-10 10:32:30 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -12,15 +12,18 @@
 // };
 import mongoose from "mongoose";
 
-export const connectDB = async (uri) => {
+const connectDB = async () => {
   try {
-    await mongoose.connect(uri);
-    console.log("MongoDB Connected Successfully.");
-  } catch (err) {
-    console.error("MongoDB Connection Error:", err.message);
+    console.log("DEBUG MONGO_URI:", process.env.MONGO_URI);
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
     process.exit(1);
   }
-};
+};export default connectDB;
 
-
-export default connectDB;
